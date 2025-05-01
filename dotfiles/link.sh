d6a893c4c78f820a792a8e5001e283e2760089c5
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2025-05-01 20:20:06 +0200

    gcd ssh agent dotfiles link

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -10,3 +10,9 @@ ln -sTf $dir/wlogout $HOME/.config/wlogout && echo "Linked wlogout"
 ln -s $dir/mimepps.list $HOME/.config/mimeapps.list && echo "Linked mimeapps"
 ln -s $dir/.asloftrc $HOME/.alsoftrc && echo "Linked mimeapps"
 ln -sTf $dir/cliphist $HOME/.config/cliphist && echo "Linked cliphist"
+
+mkdir -p $HOME/.config/systemd/user/sockets.target.wants
+ln -s $dir/systemd/user/sockets.target.wants/gcr-ssh-agent.socket $HOME/.config/systemd/user/sockets.target.wants/gcr-ssh-agent.socket && echo "Linked gcr ssh agent"
+echo "Enabling gcr ssh agent service"
+systemctl --user enable gcr-ssh-agent.service
+systemctl --user start gcr-ssh-agent.service
