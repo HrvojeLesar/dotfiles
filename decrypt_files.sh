5cecc0a2367c905119f4733951f5910e5c873ed7
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2026-04-16 22:51:36 +0200

    Removed unused configs

--- decrypt_files.sh
+++ decrypt_files.sh
@@ -2,6 +2,3 @@
 
 ./scripts/decrypt.sh ./dotfiles/hypr/wallpapers
 ./scripts/decrypt.sh ./dotfiles/ssh
-./scripts/decrypt.sh ./dotfiles/wojk/openvpn
-./scripts/decrypt.sh ./dotfiles/wojk/ssh
-./scripts/decrypt.sh ./dotfiles/wojk/systemd
