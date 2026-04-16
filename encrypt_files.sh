2379b9fd98f9a637ab399b64dabc3f96ec91461f
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2026-04-16 22:51:36 +0200

    Removed unused configs

--- encrypt_files.sh
+++ encrypt_files.sh
@@ -2,6 +2,3 @@
 
 ./scripts/encrypt.sh ./dotfiles/hypr/wallpapers
 ./scripts/encrypt.sh ./dotfiles/ssh
-./scripts/encrypt.sh ./dotfiles/wojk/openvpn
-./scripts/encrypt.sh ./dotfiles/wojk/ssh
-./scripts/encrypt.sh ./dotfiles/wojk/systemd
